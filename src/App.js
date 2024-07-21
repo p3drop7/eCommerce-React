e4721d1e5c7239e56530c10210e6b0043f67e81e
Pedro Palencia <pedropalencia7@gmail.com>
2024-07-21 19:19:35 +0100

    onLoad event added

--- src/App.js
+++ src/App.js
@@ -1,45 +1,35 @@
-import './App.css';
-import { CartProvider } from './context/CartContext.jsx';
-import ProductCardContainer from './components/ProductCard/ProductCardContainer';
-import useProducts from './data/useProducts';
-import NavBar from './components/NavBar/NavBar';
-import Cart from './components/Cart/Cart';
-
+import "./App.css";
+import { CartProvider } from "./context/CartContext.jsx";
+import ProductCardContainer from "./components/ProductCard/ProductCardContainer";
+import useProducts from "./data/useProducts";
+import NavBar from "./components/NavBar/NavBar";
+import Cart from "./components/Cart/Cart";
 
 function App() {
-
   const { products, refreshProducts } = useProducts();
 
-  const handleClick =()=> {
-    refreshProducts()
-  }
+  const handleLoad = () => {
+    refreshProducts();
+  };
 
   return (
-    <div className="App">
+    <div className="App" onLoad={handleLoad()}>
       <h1>RCS - React Clothing Store</h1>
-        <CartProvider>
-      <section>
-          <Cart/>
-      </section>
+      <CartProvider>
+        <section>
+          <Cart />
+        </section>
 
-      <NavBar/>
+        <NavBar />
 
-      <header id="home-header">HEADER</header>
+        <header id="home-header">HEADER</header>
 
-      <button onClick={handleClick}>REFRESH DATA</button>
-      
         <ProductCardContainer products={products} />
       </CartProvider>
 
-
-
       <section id="about">ABOUT</section>
 
-      <footer>
-        FOOTER
-      </footer>
-
-
+      <footer>FOOTER</footer>
     </div>
   );
 }
